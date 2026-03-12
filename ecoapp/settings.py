80605ad39f8f35060236057e23e73ffe2a0528e9
Marcelo Arteaga <arteagamarcelo@gmail.com>
2026-03-11 22:14:05 -0400

    clase 2

--- ecoapp/settings.py
+++ ecoapp/settings.py
@@ -79,6 +79,16 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "usipm5v8",
+#         "USER": "postgres",
+#         "PASSWORD": "Developer",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     }
+# }
 
 
 # Password validation
--- inventario/admin.py
+++ inventario/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Categoria, Producto
+
+admin.site.register(Categoria)
+
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'categoria', 'unidades', 'precio')
+    ordering = ('precio',)
+    search_fields = ('nombre',)
+    list_filter = ('unidades',)
+
+admin.site.register(Producto, ProductoAdmin)
